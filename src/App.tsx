b2b65423a7c6c10c09ef60b133be605ebeac264e
zujiaozhang <859166618@qq.com>
2021-04-27 20:37:40 +0800

    使用json-server 中间件 middleware

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@
  * @Descripttion: test
  * @Date: 2021-04-26 15:53:01
  * @LastEditors: love-coding
- * @LastEditTime: 2021-04-27 16:13:08
+ * @LastEditTime: 2021-04-27 19:20:27
  */
 import React from 'react';
+import Login from 'screens/login';
 import { ProjectListScreen } from 'screens/project-list';
 import { TsReactTest } from 'try-use-array';
 import './App.css';
@@ -13,7 +14,8 @@ function App() {
   return (
     <div className="App">
       {/* <ProjectListScreen/> */}
-      <TsReactTest/>
+      {/* <TsReactTest/> */}
+      <Login/>
     </div>
   );
 }
---
+++ src/screens/login/index.tsx
@@ -0,0 +1,44 @@
+/*
+ * @Descripttion: test
+ * @Date: 2021-04-27 19:06:42
+ * @LastEditors: love-coding
+ * @LastEditTime: 2021-04-27 20:02:35
+ */
+import React, { FormEvent} from 'react';
+const apiUrl = process.env.REACT_APP_API_URL;
+const Login = () => {
+	const login = (param:{username:string, password:string})=>{
+		fetch(`${apiUrl}/login`,{
+			method: 'POST',
+			headers: {
+				"Content-Type":"application/json"
+			},
+			body: JSON.stringify(param)
+		}).then(async (response) => {
+			if (response.ok) {
+				
+			}
+		});
+	}
+    const handleSubmit=(event:FormEvent<HTMLFormElement>)=>{
+		event.preventDefault();
+		const username = (event.currentTarget.elements[0] as HTMLInputElement).value
+		const password = (event.currentTarget.elements[1] as HTMLInputElement).value
+		login({username,password})
+	}
+	return (
+		<form onSubmit={handleSubmit}>
+			<div>
+				<label htmlFor="username">用户名</label>
+				<input type="text" id="username" />
+			</div>
+			<div>
+				<label htmlFor="password">密码</label>
+				<input type="password" name="" id="password" />
+			</div>
+			<button type="submit">登录</button>
+		</form>
+	);
+};
+
+export default Login;
