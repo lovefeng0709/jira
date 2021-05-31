42a8a640d04f8be38f8cfe14c6be7c02ad97e1dc
zujiaozhang <859166618@qq.com>
2021-05-31 15:04:10 +0800

    用use-select.tsx选择用户+

--- src/screens/project-list/util.ts
+++ src/screens/project-list/util.ts
@@ -2,11 +2,11 @@
  * @Descripttion: test
  * @Date: 2021-05-31 14:34:00
  * @LastEditors: love-coding
- * @LastEditTime: 2021-05-31 14:56:28
+ * @LastEditTime: 2021-05-31 15:02:35
  */
 import { useMemo } from "react"
 import { useUrlQueryParam } from "utils/url"
-
+// 项目列表搜索的参数
 export const useProjectsSearchParams = ()=>{
 
     const [param,setParam]= useUrlQueryParam(['name', 'personId'])
